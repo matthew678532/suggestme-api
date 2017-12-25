bc9208f3ce0ce2906d388c5ebac8cec9501c9063
Matthew Birch <matthew678532@gmail.com>
2017-12-25 20:06:33 +0000

    [suggestme-0013] - outlined template files for modules, and test scripts

---
+++ templates/module_template.js
@@ -0,0 +1,14 @@
+import * as { module } from { package };
+import { module } from { package };
+
+export function { methodName }(...params) {
+  return new Promise((resolve, reject) => {
+    // Function Body...
+  })
+}
+
+export default class { className } {
+  constructor(opts) {
+    super(opts);
+  }
+}
