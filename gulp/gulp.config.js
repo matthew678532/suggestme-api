1625ce611bbf830bf948919e8ca9e72ca388b4f1
Matthew Birch <matthew678532@gmail.com>
2018-01-01 09:12:05 +0000

    [suggestme-0017] - implemented a base default task using the task names stored within the gulp.config file

--- gulp/gulp.config.js
+++ gulp/gulp.config.js
@@ -1,11 +1,12 @@
 import { root } from './../global.config.js'
 
 export default {
-  // Config options go here
+  // File path globs
   SRC_MODULE: `${root}/src/module/**/*.js`,
   DIST: `${root}/dist`,
   NODE_MODULES: `${root}/node_modules/**`,
   ESLINT_CONFIG: `${root}/.eslintrc.js`,
+  // Task names
   JS_TASK: 'js',
   ESLINT_TASK: 'eslint'
 }
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,10 @@
 
 import * as tasks from './gulp/tasks.js'
 import gulp from 'gulp'
+import config from './gulp/gulp.config.js'
 
 Object.keys(tasks).forEach((taskName) => {
   gulp.task(taskName, tasks[taskName])
 })
 
-gulp.task('default', Object.keys(tasks))
+gulp.task('default', gulp.series(config.ESLINT_TASK, config.JS_TASK))
