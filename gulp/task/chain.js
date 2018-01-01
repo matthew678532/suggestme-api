52fed3b87bcc3af1263d69c58a4dbeb6aa4e43f4
Matthew Birch <matthew678532@gmail.com>
2018-01-01 09:16:27 +0000

    [suggestme-0017] - removed redundant default (chain) task

--- gulp/task/chain.js
+++
@@ -1,5 +0,0 @@
-import { config, gulp } from './../core.js'
-
-export default function chain() {
-  gulp.series(config.ESLINT_TASK, config.JS_TASK)
-}
--- gulp/tasks.js
+++ gulp/tasks.js
@@ -1,6 +1,5 @@
 import js from './task/js.js'
 import eslint from './task/eslint.js'
-import chain from './task/chain.js'
 // etc
 
-export { js, eslint, chain }
+export { js, eslint }
