7ba3aba3009ae6a6e565355b6b347aaf011d773c
Matthew Birch <matthew678532@gmail.com>
2018-01-07 19:38:56 +0000

    [suggestme-0017] - defined some global utility methods to aid gulp utility developments

---
+++ gulp/util.js
@@ -0,0 +1,17 @@
+/**
+ * @module gulp/util
+ * @author Matthew Birch <matthew678532@gmail.com>
+ */
+
+/**
+ * getTaskNames - get the task names of
+ * the gulp tasks.
+ * @param  {Object} tasks an object containing
+ * a collection of gulp tasks defined within
+ * gulp/task.
+ * @return {Array} an empty array if no tasks
+ * exist, otherwise an
+ */
+export function getTaskNames(tasks) {
+  // todo
+}
--- src/helper/util.js
+++ src/helper/util.js
@@ -1,4 +1,12 @@
 export default class Util {
+  static isPureObject(obj) {
+    return !this.isArray(obj)
+           && !this.isString(obj)
+           && !this.isNumber(obj)
+           && !this.isBoolean(obj)
+           && !this.isFunction(obj)
+  }
+
   static isObject(obj) {
     return Object.prototype.toString.call(obj) === '[object Object]'
   }
@@ -13,5 +21,13 @@ export default class Util {
 
   static isNumber(num) {
     return Object.prototype.toString.call(num) === '[object Number]'
+  }
+
+  static isBoolean(bool) {
+    return Object.prototype.toString.call(bool) === '[object Boolean]'
+  }
+
+  static isFunction(func) {
+    return Object.prototype.toString.call(func) === '[object Function]'
   }
 }
